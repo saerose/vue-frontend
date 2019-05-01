0a88d3020dcc843a528135a411d18aaf5bbc2ca2
saerose <sbarcelocompte@gmail.com>
2019-05-01 11:07:05 +0200

    feat: created component for login button

---
+++ src/components/login/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <button class="login">Login</button>
+</template>
+
+<style lang="sass" scoped>
+  @import '../../styles'
+  .login
+    width: 5vw
+    height: 5vh
+    background-color: $magenta
+    color: $white
+    font-weight: 700
+    text-transform: uppercase
+    border: none
+</style>
