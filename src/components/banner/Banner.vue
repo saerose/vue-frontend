4835bab0ee2cbe92995e61d87dacd64b9b082230
saerose <sbarcelocompte@gmail.com>
2019-05-01 15:27:29 +0200

    chore: sass on banner

--- src/components/banner/Banner.vue
+++ src/components/banner/Banner.vue
@@ -1,12 +1,17 @@
 <template>
-  <div class="banner">
-    <div class="banner__list" v-for="film in films" :style="film.backdrop_path">
-      <div class="banner__list-title">{{film.original_title}}</div>
-      <div class="banner__list-info">Movie info</div>
-      <div class="banner__list-buttons">
-        <button>Play Film</button>
-        <button>Trailer</button>
-        <button>Download</button>
+  <div class="banner__list">
+    <div class="banner__list-element" v-for="film in films" :style="film.backdrop_path">
+      <div class="banner__list-element__content">
+        <div class="banner__list-element__content-title">{{film.original_title}}</div>
+        <div class="banner__list-element__content-info">
+          <span class="banner__list-element__content-info__date"><b>Release date: </b>{{film.release_date}}</span>
+          <span class="banner__list-element__content-info__overview">{{film.overview}}</span>
+        </div>
+        <div class="banner__list-element__content-buttons">
+          <button class="banner__list-element__content-buttons__element">Play Film</button>
+          <button class="banner__list-element__content-buttons__element">Trailer</button>
+          <button class="banner__list-element__content-buttons__element">Download</button>
+        </div>
       </div>
     </div>
   </div>
@@ -14,13 +19,14 @@
 
 <style lang="sass" scoped>
   @import '../../styles'
-  .banner
+  .banner__list
     +flex ()
     overflow-x: auto
     width: 100vw
-    height: 50vh
+    height: 70vh
     margin: 0 calc(50% - 50vw)
-    .banner__list
+    .banner__list-element
+      z-index: -1
       flex: 0 0 auto
       width: 100%
       padding: 1vw
@@ -28,12 +34,35 @@
       background-position: center
       background-size: cover
       animation: iterateBanner 50s steps(10, end) infinite
-      .banner__list-title
-        font-weight: 700
-        font-size: 3em
-      .banner__list-info
-        font-size: 1em
-      .banner__list-buttons
+      color: $white
+      .banner__list-element__content
+        position: absolute
+        bottom: 0
+        margin-bottom: 4vh
+        font-family: 'Source Sans Pro', sans-serif
+        .banner__list-element__content-title
+          font-weight: 700
+          font-size: 3em
+        .banner__list-element__content-info
+          +flex($direction: column)
+          width: 25vw
+          margin-bottom: 1vh
+          .banner__list-element__content-info__date
+            font-size: 0.8em
+          .banner__list-element__content-info__overview
+            font-size: 0.9em
+
+        .banner__list-element__content-buttons
+          .banner__list-element__content-buttons__element
+            width: 14vw
+            height: 7vh
+            background-color: transparent
+            border: 0.1em solid $white
+            color: $white
+            text-transform: none
+            font-weight: 300
+            margin: 0 3vw 0 0
+            font-size: 1.5em
 
   @keyframes iterateBanner
     from
