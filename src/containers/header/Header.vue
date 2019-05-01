bcba960665e50aeaab09a5c1df88366d59a1657f
saerose <sbarcelocompte@gmail.com>
2019-05-01 10:41:31 +0200

    feat: added header container

---
+++ src/containers/header/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="header">
+    <h1 class="header__title">This is the header</h1>
+    <button class="header__button">Login ✨</button>
+  </div>
+</template>
+
+<style>
+  .header {
+    font-family: 'Source Sans Pro', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+</style>
