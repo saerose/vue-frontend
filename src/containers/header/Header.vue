9a5d3a4baac1b1ae0d4b560a038560f25cd4b651
saerose <sbarcelocompte@gmail.com>
2019-05-01 11:07:22 +0200

    chore: modified header to add new login button

--- src/containers/header/Header.vue
+++ src/containers/header/Header.vue
@@ -1,14 +1,36 @@
 <template>
   <div class="header">
-    <h1 class="header__title">This is the header</h1>
-    <button class="header__button">Login ✨</button>
+    <h1 class="header__title">
+      <span class="header__title-xconfessions">XConfessions</span><span class="header__title-by"> by you & Erika Lust</span>
+    </h1>
+    <Login></Login>
   </div>
 </template>
 
-<style>
-  .header {
-    font-family: 'Source Sans Pro', sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
+<style lang="sass" scoped>
+  @import '../../styles'
+  .header
+    +flex ($direction: row, $justify: space-between, $align: center)
+    font-family: 'Source Sans Pro', sans-serif
+    -webkit-font-smoothing: antialiased
+    -moz-osx-font-smoothing: grayscale
+    padding: 1vw
+    .header__title
+      font-size: 1.5em
+      color: $magenta
+      .header__title-xconfessions
+        font-weight: 700
+      .header__title-by
+        font-weight: 300
 </style>
+
+<script>
+  import Login from '../../components/login/Login.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      Login
+    }
+  }
+</script>
